3051258371553f4437b013288e88d0ca03013460
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-05-16 00:41:53 +0900

    Initial Commit

---
+++ ScanApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanApp
+//
+//  Created by Yoshihiro Uda on 2020/05/16.
+//  Copyright © 2020 Yoshihiro Uda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
