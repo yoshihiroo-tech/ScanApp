61eec54117571443377e469c85891d3302ac6315
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-05-16 22:32:06 +0900

    create cardVC UI

--- ScanApp/ViewController.swift
+++ ScanApp/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import LineSDK
+import Photos
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var lineLoginButton: LoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //ユーザーに許可を促す
+        PHPhotoLibrary.requestAuthorization { (status) in
+            switch(status){
+                
+            case .authorized:
+                break
+                
+            case .denied:
+                break
+                
+            case .notDetermined:
+                break
+                
+            case .restricted:
+                break
+            }
+        }
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //ナビバーを消す
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
 
+    
+    @IBAction func loginAction(_ sender: Any) {
+        
+        //ログインを実装
+        LoginManager.shared.login(permissions: [.profile], in: self) { (result) in
+            
+            switch result{
+                
+            case .success(let loginResult):
+                if let profile = loginResult.userProfile{
+                    
+                    UserDefaults.standard.set(profile.displayName, forKey: "displayName")
+                    UserDefaults.standard.set(String(describing: profile.pictureURL), forKey: "pictureURLString")
+                    
+                    //画面遷移
+            
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
 }
 
